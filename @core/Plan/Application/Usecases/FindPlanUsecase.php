0dab9b2097a83371bcae10f4b6af869790a4a51c
Eduardo Cavalcante <eduardo.ps.cavalcante@gmail.com>
2024-03-28 07:58:56 -0400

    feat: plan - application find usecase and tests

---
+++ @core/Plan/Application/Usecases/FindPlanUsecase.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Core\Plan\Application\Usecases;
+
+use Core\Plan\Application\Usecases\Dto\FindPlanUsecaseInput;
+use Core\Plan\Application\Usecases\Dto\FindPlanUsecaseOutput;
+use Core\Plan\Domain\Exceptions\PlanNotFoundException;
+use Core\Plan\Domain\Repositories\PlanRepositoryInterface;
+use Core\Shared\Domain\Uuid;
+
+class FindPlanUsecase
+{
+    public function __construct(
+        private readonly PlanRepositoryInterface $planRepository
+    ) {
+    }
+
+    public function execute(FindPlanUsecaseInput $input)
+    {
+        $planId = new Uuid(($input->id));
+        $plan = $this->planRepository->findById($planId);
+
+        if (is_null($plan)) throw new PlanNotFoundException($planId);
+
+        return FindPlanUsecaseOutput::build($plan);
+    }
+}
---
+++ tests/Unit/Core/Application/Usecases/FindPlanUsecaseTest.php
@@ -0,0 +1,76 @@
+<?php
+
+use Core\Plan\Application\Usecases\Dto\FindPlanUsecaseInput;
+use Core\Plan\Application\Usecases\Dto\FindPlanUsecaseOutput;
+use Core\Plan\Application\Usecases\FindPlanUsecase;
+use Core\Plan\Domain\Exceptions\PlanNotFoundException;
+use Core\Plan\Domain\Plan;
+use Core\Plan\Domain\Repositories\PlanRepositoryInterface;
+use Core\Plan\Infra\Repositories\MemoryPlanRepository;
+use Core\Shared\Domain\Uuid;
+
+describe('FindPlanUsecase Unit Test', function () {
+    test('Deve encontrar um plano', function () {
+        $plan = new Plan(name: 'test', description: 'Alguma descricao');
+        $planRepository = new MemoryPlanRepository();
+        $planRepository->create($plan);
+        $usecase = new FindPlanUsecase(
+            planRepository: $planRepository
+        );
+        $input = new FindPlanUsecaseInput(
+            id: $plan->getId()->getValue()
+        );
+        $output = $usecase->execute($input);
+        expect($output)->toBeInstanceOf(FindPlanUsecaseOutput::class);
+        expect($output->planId)->toBe($plan->getId()->getValue());
+        expect($output->name)->toBe('test');
+        expect($output->description)->toBe('Alguma descricao');
+    });
+
+    test('Deve lançar PlanNotFoundException quando não encontrar um plano', function () {
+        $planRepository = new MemoryPlanRepository();
+        $usecase = new FindPlanUsecase(
+            planRepository: $planRepository
+        );
+        $input = new FindPlanUsecaseInput(
+            id: Uuid::create()->getValue()
+        );
+        $usecase->execute($input);
+    })->throws(PlanNotFoundException::class);
+
+    test('Deve encontrar um plano usando mock', function () {
+        $plan = new Plan(name: 'test', description: 'Alguma descricao');
+        $mockRepository = Mockery::mock(PlanRepositoryInterface::class);
+        $mockRepository->shouldReceive('findById')
+            ->with($plan->getId()->getValue())
+            ->once()
+            ->andReturn($plan);
+        $usecase = new FindPlanUsecase(
+            planRepository: $mockRepository
+        );
+        $input = new FindPlanUsecaseInput(
+            id: $plan->getId()->getValue()
+        );
+        $output = $usecase->execute($input);
+        expect($output)->toBeInstanceOf(FindPlanUsecaseOutput::class);
+        expect($output->planId)->toBe($plan->getId()->getValue());
+        expect($output->name)->toBe('test');
+        expect($output->description)->toBe('Alguma descricao');
+    });
+
+    test('Deve lançar exception PlanNotFoundException ao buscar plano usando mock', function () {
+        $planId = Uuid::create();
+        $mockRepository = Mockery::mock(PlanRepositoryInterface::class);
+        $mockRepository->shouldReceive('findById')
+            ->with($planId->getValue())
+            ->once()
+            ->andReturnNull();
+        $usecase = new FindPlanUsecase(
+            planRepository: $mockRepository
+        );
+        $input = new FindPlanUsecaseInput(
+            id: $planId->getValue()
+        );
+        $output = $usecase->execute($input);
+    })->throws(PlanNotFoundException::class);
+});
