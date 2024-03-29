6a036a8a3e7c632bd5d9e569e0fca84fee2a5be1
Eduardo Cavalcante <eduardo.ps.cavalcante@gmail.com>
2024-03-28 23:05:33 -0400

    refactor: remove tests use memory repo

--- tests/Unit/Core/Plan/Application/Usecases/FindPlanUsecaseTest.php
+++ tests/Unit/Core/Plan/Application/Usecases/FindPlanUsecaseTest.php
@@ -6,38 +6,9 @@ use Core\Plan\Application\Usecases\FindPlanUsecase;
 use Core\Plan\Domain\Exceptions\PlanNotFoundException;
 use Core\Plan\Domain\Plan;
 use Core\Plan\Domain\Repositories\PlanRepositoryInterface;
-use Core\Plan\Infra\Repositories\MemoryPlanRepository;
 use Core\Shared\Domain\Uuid;
 
 describe('FindPlanUsecase Unit Test', function () {
-    test('Deve encontrar um plano', function () {
-        $plan = new Plan(name: 'test', description: 'Alguma descricao');
-        $planRepository = new MemoryPlanRepository();
-        $planRepository->create($plan);
-        $usecase = new FindPlanUsecase(
-            planRepository: $planRepository
-        );
-        $input = new FindPlanUsecaseInput(
-            id: $plan->getId()->getValue()
-        );
-        $output = $usecase->execute($input);
-        expect($output)->toBeInstanceOf(FindPlanUsecaseOutput::class);
-        expect($output->planId)->toBe($plan->getId()->getValue());
-        expect($output->name)->toBe('test');
-        expect($output->description)->toBe('Alguma descricao');
-    });
-
-    test('Deve lançar PlanNotFoundException quando não encontrar um plano', function () {
-        $planRepository = new MemoryPlanRepository();
-        $usecase = new FindPlanUsecase(
-            planRepository: $planRepository
-        );
-        $input = new FindPlanUsecaseInput(
-            id: Uuid::create()->getValue()
-        );
-        $usecase->execute($input);
-    })->throws(PlanNotFoundException::class);
-
     test('Deve encontrar um plano usando mock', function () {
         $plan = new Plan(name: 'test', description: 'Alguma descricao');
         $mockRepository = Mockery::mock(PlanRepositoryInterface::class);
