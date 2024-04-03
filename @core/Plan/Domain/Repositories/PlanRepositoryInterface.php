0fe598664d2ca28dcc457ef7d21b2a80cb1ac644
Eduardo Cavalcante <eduardo.ps.cavalcante@gmail.com>
2024-04-03 08:44:57 -0400

    ajustes no repositorio do plan

--- @core/Plan/Domain/Repositories/PlanRepositoryInterface.php
+++ @core/Plan/Domain/Repositories/PlanRepositoryInterface.php
@@ -2,24 +2,12 @@
 
 namespace Core\Plan\Domain\Repositories;
 
-use Core\Plan\Domain\Plan;
-use Core\Shared\Domain\Uuid;
+use Core\Shared\Domain\Repositories\RepositoryInterface;
 
-interface PlanRepositoryInterface
+interface PlanRepositoryInterface extends RepositoryInterface
 {
-    public function create(Plan $entity): void;
-
-    public function update(Plan $entity): void;
-
-    public function delete(Uuid $id): void;
-
-    public function findById(Uuid $id): ?Plan;
-
-    public function search(
-        ?string $filterBy = null,
-        ?string $sortBy = null,
-        ?string $sortDir = null,
-        ?int $page = 1,
-        ?int $perPage = 10,
-    ): SearchResultInterface;
+    /**
+     * @param  Plan  $entity
+     */
+    public function create(object $entity): void;
 }
